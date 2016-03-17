6cae4b1d18184f0008bc86e9e73c148de80cd08d
Oleg Dreyman <dreymonde@me.com>
2016-03-17 12:18:32 +0200

    Add Jay 0.3.0 for Linux

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 	let package = Package(
 		name: "EezehRequests",
 		dependencies: [
-			.Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 3)
+			.Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 3),
+			.Package(url: "https://github.com/czechboy0/Jay.git", Version(0,3,0))
 		]
 	)
 #else
